f0e4cbe2e5ee1da1f70b78a74ec501799acd5cb8
gabrielMina <gabrielmina118@gmail.com>
2021-06-04 18:31:36 -0300

    endpoint de criar turma

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
-import { Request, Response } from "express";
 import app from "./app";
-import { connection } from './connection'
+import getTurma from "./endpointData/getTurma";
 
 
-app.put("/turma",(req:Request,res:Response)=>{
-    try {
-        const{nome,data_inicio,data_final,modulo} = req.body;
-
-        
-
-    } catch (error) {
-     res.status(400).send({message:error.message})   
-    }
-})
+app.put("/turma",getTurma)
